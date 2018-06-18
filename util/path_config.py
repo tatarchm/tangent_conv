6f4918ee5d3b7e34ee6fa94883a8130bddaa94c1
Maxim Tatarchenko <tatarchm@gmail.com>
2018-06-18 11:26:51 +0200

    Update path_config.py

--- util/path_config.py
+++ util/path_config.py
@@ -1,7 +1,7 @@
 import sys
 
-open3d_path = '/misc/lmbraid17/tatarchm/projects/tangent_conv/dev/Open3D/build/lib/'
-tc_path = '/misc/lmbraid17/tatarchm/projects/tangent_conv/dev/tangent_convolutions/'
+open3d_path = '<your_path>/Open3D/build/lib/'
+tc_path = '<your_path>/tangent_convolutions/'
 
 sys.path.append(open3d_path)
 from py3d import *
